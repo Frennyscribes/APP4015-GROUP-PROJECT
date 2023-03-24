e5f6a81f9caa95641f9ec0fdd898c3714264b80c
Frennyscribes <lnjirun@usiu.ac.ke>
2023-03-24 19:21:03 +0300

    Application Interface with changes by presentation

---
+++ src/main/java/com/mycompany/chatapplicationproject/AdminC.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatapplicationproject;
+
+/**
+ *
+ * @author Hp
+ */
+public class AdminC extends User{
+    String role;
+   
+    public void editEvent()
+    {
+        
+	
+    }
+    public void addParticipant()
+    {
+        
+	
+    }
+    public void removeParticipant()
+    {
+        
+	
+    }
+    
+    
+
+}
